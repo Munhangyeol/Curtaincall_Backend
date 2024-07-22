0ae6ee6ea89baf5c0c15c3cfb9c3e561be1b80e8
Munhangyeol <mhg10181018@gmail.com>
2024-07-22 17:05:58 +0900

    [BE#3-1] feat: domain 설계 완료 및 패키지 구조 설계 완료

---
+++ curtaincall/src/main/java/com/example/curtaincall/domain/PhoneBook.java
@@ -0,0 +1,24 @@
+package com.example.curtaincall.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class PhoneBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickName;
+
+    private String phoneNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    
+}
